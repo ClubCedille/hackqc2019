c7663de30017290795a18d8a8c4522261948c0a5
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2019-03-09 12:24:19 -0500

    Le JSON s'est modifié tout seul!

--- server/routes/coordgps.js
+++ server/routes/coordgps.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 // Example route -> GET /example
 router.get('/', (req, res) => {
-  res.json({ "lat": 45.493720, "long": -73.564130});
+  res.json({ lat: 45.49372, long: -73.56413 });
 });
 
 export default router;

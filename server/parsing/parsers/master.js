8da56f0b581aa25486d25b260f675c84af267228
guertz <charlesguertin@live.ca>
2019-03-08 20:48:23 -0500

    changed little things

--- server/parsing/parsers/master.js
+++ server/parsing/parsers/master.js
@@ -1,6 +1,6 @@
 export default class Parser {
   async parse() {
     // Parse definition goes in sub-classes.
-    throw new Error("You must implement this method.");
+    throw new Error('You must implement this method.');
   }
 }

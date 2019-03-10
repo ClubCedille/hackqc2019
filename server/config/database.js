c95229a6545063da322acdaf911effc1d7a5cb35
guertz <charlesguertin@live.ca>
2019-03-09 22:25:50 -0500

    Changes to database connection

--- server/config/database.js
+++ server/config/database.js
@@ -4,22 +4,12 @@ const {
   POSTGRES_USER,
   POSTGRES_PASSWORD,
   POSTGRES_HOST,
-  POSTGRES_URL,
 } = process.env;
 
-const sequelize = new Sequelize(POSTGRES_URL, {
+const sequelize = new Sequelize(POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
   host: POSTGRES_HOST,
   dialect: 'postgres',
   port: 5432,
-  operatorsAliases: false,
-  dialectOptions: {
-    ssl: 'Amazon RDS',
-  },
-}); /*POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD, {
-  host: POSTGRES_HOST,
-  dialect: 'postgres',
-  port: 5432,
-  protocol: null,
   operatorsAliases: false,
   pool: {
     max: 5,
@@ -27,30 +17,7 @@ const sequelize = new Sequelize(POSTGRES_URL, {
     idle: 20000,
     acquire: 20000,
   },
-  retry: {
-    match: [
-      /ETIMEDOUT/,
-      /EHOSTUNREACH/,
-      /ECONNRESET/,
-      /ECONNREFUSED/,
-      /ETIMEDOUT/,
-      /ESOCKETTIMEDOUT/,
-      /EHOSTUNREACH/,
-      /EPIPE/,
-      /EAI_AGAIN/,
-      /SequelizeConnectionError/,
-      /SequelizeConnectionRefusedError/,
-      /SequelizeHostNotFoundError/,
-      /SequelizeHostNotReachableError/,
-      /SequelizeInvalidConnectionError/,
-      /SequelizeConnectionTimedOutError/,
-    ],
-    max: 5,
-  },
-  dialectOptions: {
-    ssl: 'Amazon RDS',
-  },
-});*/
+});
 
 sequelize
   .authenticate()
